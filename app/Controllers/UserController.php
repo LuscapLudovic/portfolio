8eb69aaca87266259a769d225ce880d0f21ace55
Ludo <kiluhasenpai@gmail.com>
2019-03-04 20:17:52 +0100

    avec les commentaires c'est mieux

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -8,11 +8,18 @@ use Respect\Validation\Validator;
 
 class UserController extends Controller {
 
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @return mixed
+     */
     public function newUser(RequestInterface $request, ResponseInterface $response){
         $errors = [];
         Validator::email()->validate($request->getParam('email')) || $errors['email'] = 'Votre email n\'est pas valide';
         Validator::notEmpty()->validate($request->getParam('login')) || $errors['login'] = 'Veuillez entrer votre login';
+        // si tu trouve une solution pour une regex sur le mdp t'es un génie mon kyky
         Validator::notEmpty()->validate($request->getParam('mdp')) || $errors['mdp'] = 'Veuillez entrer un mot de passe';
+        // la fonction "flash" est dans le controller c'est juste un notif
         if(empty($errors)){
             $prepare = $this->pdo()->prepare('INSERT INTO users(login, email, mdp) VALUES'
             .'('.$request->getParam('login').','.$request->getParam('email').','.$request->getParam('mdp').')');
@@ -21,10 +28,14 @@ class UserController extends Controller {
         } else {
             $this->flash('Certains champs n\'ont pas été rempli correctement ', 'error');
             $this->flash($errors, 'errors');
+            //si tu as une page blanche au redirect change le status à 200 ou enleve le tout court.
             return $this->redirect($response,'incription', 400);
         }
     }
 
+    /**
+     * c'est une fonction pour test si la connexion marche bien
+     */
     public function testUser(){
         $prepare = $this->pdo()->prepare('SELECT * FROM  users');
         $prepare->execute();
